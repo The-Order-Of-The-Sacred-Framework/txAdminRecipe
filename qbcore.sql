3789fcc04d555e1e60c347313defb9345892eaed
MrNewb <47620135+MrNewb@users.noreply.github.com>
2025-05-07 16:46:25 -0400

    sql
    sql

--- qbcore.sql
+++ qbcore.sql
@@ -227,6 +227,8 @@ CREATE TABLE IF NOT EXISTS `players` (
   KEY `license` (`license`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
+ALTER TABLE `players` ADD `crafting_blueprints` LONGTEXT NOT NULL DEFAULT '[]';
+
 CREATE TABLE IF NOT EXISTS `playerskins` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `citizenid` VARCHAR(11) NOT NULL,
